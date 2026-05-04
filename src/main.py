a7707cdf95c1d5923ce26136e585011969e493c2
alfux <alexis.t.fuchs@gmail.com>
2026-05-04 10:54:00 +0200

    typing

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ def get_args(description: str = '') -> Namespace:
     av.add_argument("--board-size", default=10, type=int, help=message)
     av.add_argument("--savepath", default=None, help="Save path")
     message = "Override maximum threads. May cause instabilities. But funny."
-    av.add_argument("--max-thread", default=1, help=message)
+    av.add_argument("--max-thread", default=1, type=int, help=message)
     av.add_argument("--debug", action="store_true", help="Traceback mode.")
     return av.parse_args()
 
